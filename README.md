90fe7114e0d7bae2a354b56ea5b28f4384cc812a
Alexandra Hoefinger <alexandra.hoefinger@gmail.com>
2015-04-01 11:40:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 example
 -------
 
-```
-user@machine:~/python-homepage-templates$ python main.py resources/women.csv 140526 > women_page.html
+Run the batch:
+
+user@machine:~/python-hp-gw-script$ bash run_batch.sh yymmdd
 ```
---
+++ run_batch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python main.py 'a' 'APARTMENT' $1
+python main.py 'w' 'WOMENS' $1
+python main.py 'b' 'BEAUTY' $1
+python main.py 'm' 'MENS' $1
+python main.py 'mu' 'MUSIC' $1
+# python main.py 'ww' 'WW' $1
+# python main.py 's' 'SALE' $1
